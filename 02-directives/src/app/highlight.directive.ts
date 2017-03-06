158f7294e2d701100c3590a8106f23120d5cd353
schemr <schemryj@gmail.com>
2017-03-06 14:34:03 +0900

    Input property bind 테스트

--- 02-directives/src/app/highlight.directive.ts
+++ 02-directives/src/app/highlight.directive.ts
@@ -1,24 +1,29 @@
-import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding, Input, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[dirHighlight]'
+  selector: '[highlight]'
 })
 export class HighlightDirective {
   @HostListener('mouseenter') onmouseover() {
-    this.backgroundColor = 'green'
+    this.backgroundColor = this.highlightColor;
   };
   @HostListener('mouseleave') onmouseleave() {
-    this.backgroundColor = 'white'
+    this.backgroundColor = this.defaultColor;
   };
   @HostBinding('style.backgroundColor') get setColor(){
     return this.backgroundColor;
   }
 
-  private backgroundColor = 'white'
+  @Input() defaultColor = 'white';
+  @Input('highlight') highlightColor = 'green';
+
+  private backgroundColor : string;
   constructor(private elementRef: ElementRef, private renderer: Renderer) {
     //첫번째 방법은 직접 DOM에 접근하므로 사용을 자제하고 두번째 방법 처럼 renderer API 를 사용하자
     //this.elementRef.nativeElement.style.backgroundColor = 'green';
     //this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'red')
   }
-
+  ngOnInit(){
+    this.backgroundColor = this.defaultColor;
+  }
 }
