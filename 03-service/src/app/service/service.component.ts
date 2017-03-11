cdf26e54d8c41887978e70d0a30e1b0e82cb96cb
schemr <schemryj@gmail.com>
2017-03-11 16:31:14 +0900

    service 스터디를 위한 세팅

---
+++ 03-service/src/app/service/service.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'si-service',
+  template: `
+    <p>
+      <si-cmp-a></si-cmp-a>
+      <si-cmp-a></si-cmp-a>
+    </p>
+  `,
+  styles: []
+})
+export class ServiceComponent {
+}
