86d510fbc78ee3bc30cbd0fdf65e68946c6ae630
schemr <schemryj@gmail.com>
2017-03-20 17:19:43 +0900

    Form Groups 추가

--- form/src/app/data-driven/data-driven.component.ts
+++ form/src/app/data-driven/data-driven.component.ts
@@ -10,11 +10,13 @@ export class DataDrivenComponent {
   myForm: FormGroup;
   constructor(){
     this.myForm = new FormGroup({
-      'username': new FormControl('Chris', Validators.required),
-      'email': new FormControl('', [
-          Validators.required, 
-          Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-        ]), 
+      'userData' : new FormGroup({
+        'username': new FormControl('Chris', Validators.required),
+        'email': new FormControl('', [
+            Validators.required, 
+            Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+        ])
+      }),
       'password': new FormControl('', Validators.required)
     });
   }
