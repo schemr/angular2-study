797d6a2edb5b2532148ae770cea9329621a434ce
schemr <schemryj@gmail.com>
2017-03-20 10:43:13 +0900

    template-driven 부트스트랩 마크업 구현

---
+++ form/src/app/template-driven/template-driven.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'template-driven',
+  templateUrl: './template-driven.component.html'
+})
+export class TemplateDrivenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
