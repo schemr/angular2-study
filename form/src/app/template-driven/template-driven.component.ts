34f4bce9fae3e998fcbdbafb41ff8dc1187dbcf2
schemr <schemryj@gmail.com>
2017-03-20 10:49:51 +0900

    부트스트랩 import

--- form/src/app/template-driven/template-driven.component.ts
+++ form/src/app/template-driven/template-driven.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'template-driven',
   templateUrl: './template-driven.component.html'
 })
-export class TemplateDrivenComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class TemplateDrivenComponent {
+  onSubmit(){
+    console.log("It works!")
   }
-
 }
