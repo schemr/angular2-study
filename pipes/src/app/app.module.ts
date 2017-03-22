4df788c00de505e34cf54f1373716f94364d6195
schemr <schemryj@gmail.com>
2017-03-22 21:21:41 +0900

    Custom Pipe(double)

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DoublePipe } from './double.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoublePipe
   ],
   imports: [
     BrowserModule,
